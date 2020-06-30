7de52d88966eb6be72c77f28da9a95a024b23fc7
Serafin Wesnidge <leaf466@gmail.com>
2020-06-30 16:25:14 -0600

    initialization

---
+++ README.md
@@ -0,0 +1,8 @@
+# Svelte Boilerplate
+
+Just a quick to get up and running svelte boilerplate
+
+# Installation
+
+1. npm install
+2. make run
---
+++ src/App.svelte
@@ -0,0 +1,55 @@
+<script>
+  const optionalChaining = {
+    value: [{
+      deeplyNested: true,
+    }, {
+      deeplyNested: () => 4 + 3,
+    }],
+    destructureNullFails: null,
+  };
+
+  const chainMe = optionalChaining?.value?.[0]?.deeplyNested;
+  const chainMeFunc = optionalChaining?.value?.[0]?.deeplyNested;
+  console.log(chainMe, chainMeFunc);
+  export let name;
+</script>
+
+<style type="text/scss">
+  $color: red;
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: $color;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+
+  div {
+    background: green;
+
+    > p {
+      color: #fff;
+    }
+  }
+</style>
+
+<main>
+  <h1>Hello {name}!</h1>
+	<h1>Hello {console.log('hey') && name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <div>
+    <p>SASS is working!</p>
+  </div>
+</main>
